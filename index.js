a73e3e211951c606fbee4a01ad3871776d9bac5e
Erik <masteret@ymail.com>
2025-03-16 14:49:48 -0400

    Implemented hover effect to reveal the punchline

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 document.getElementById('submitBtn').addEventListener('click', fetchJoke);
 
-//Loads the last joke from LocalStorage when page loads
-window.onload = function(){
-    let savedJoke = localStorage.getItem('joke');
-    if(savedJoke) {
-        document.getElementById('joke').innerText = savedJoke;
+// loads the last joke from LocalStorage when the page loads
+window.onload = function() {
+    let savedJokeSetup = localStorage.getItem('jokeSetup');
+    let savedJokePunchline = localStorage.getItem('jokePunchline');
+    if (savedJokeSetup && savedJokePunchline) {
+        document.getElementById('setup').innerText = savedJokeSetup;
+        document.getElementById('punchline').innerText = savedJokePunchline;
     }
 }
 
@@ -13,11 +15,15 @@ async function fetchJoke() {
         const response = await fetch("https://official-joke-api.appspot.com/jokes/random");
         const data = await response.json();
 
-        const joke = data.setup + " " + data.punchline + " 😂";
-        document.getElementById('joke').innerText = joke;
+        const setup = data.setup;
+        const punchline = data.punchline;
 
-        //Stores the joke in localStorage
-        localStorage.setItem('joke', joke);
+        document.getElementById('setup').innerText = setup;
+        document.getElementById('punchline').innerText = punchline;
+
+        // stores setup and punchline separately in localStorage
+        localStorage.setItem('jokeSetup', setup);
+        localStorage.setItem('jokePunchline', punchline);
 
     } catch (error) {
         document.getElementById('joke').innerText = "Oops! Couldn't fetch a joke. Try again!";
