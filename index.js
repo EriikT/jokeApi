d6af3599ffcc2fbaa6521f3aa78e05b72ae029e4
Erik <masteret@ymail.com>
2025-03-16 12:23:04 -0400

    added local storage

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-document.getElementById('submitBtn').addEventListener('click', fetchUser);
+document.getElementById('submitBtn').addEventListener('click', fetchJoke);
 
-async function fetchUser() {
+//Loads the last joke from LocalStorage when page loads
+window.onload = function(){
+    let savedJoke = localStorage.getItem('item');
+    if(savedJoke) {
+        document.getElementById('joke').innerText = savedJoke;
+    }
+}
+
+async function fetchJoke() {
     try {
         const response = await fetch("https://official-joke-api.appspot.com/jokes/random");
         const data = await response.json();
 
         const joke = data.setup + " " + data.punchline + " 😂";
         document.getElementById('joke').innerText = joke;
+
+        //Stores the joke in localStorage
+        localStorage.setItem('joke', joke);
 
     } catch (error) {
         document.getElementById('joke').innerText = "Oops! Couldn't fetch a joke. Try again!";
